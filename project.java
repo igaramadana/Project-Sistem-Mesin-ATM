849fcac2b447452a8c2ac0c7ed067f8a46baed92
IGA RAMADANA SAHPUTRA <igrmdns085@gmail.com>
2023-09-08 19:00:55 +0700

    Progress data Project

---
+++ project.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class project {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);
+
+        //Data Nasabah
+        String [] Nama = {"Iga","Annisa","Firman"};
+        String [] newNama = new String[100];
+        //Data No. Rekening
+        int[] noRek = {112233,445566,778899};
+        int[] newNoRek = new int[100];
+        //Data PIN
+        int[] Pin = {1082,7182,0611};
+        int[] newPin = new int[100];
+        //Data Alamat
+        String[] Alamat = {"Dampit","Lowokwaru","Sukun"};
+        String[] newAlamat = new String[100];
+        //Data No. Telpon
+        int[] Telp = {6281233,6282443,6284221};
+        int[] newTelp = new int[100];
+        //Data Saldo
+        int[] Saldo = {5000000,1000000,2000000};
+        int[] newSaldo = new int[100];
+        String pilih;
+        boolean pilihan = true;
+    }
+
+
+}
